47d7fbd24ddb7d2442315d66f89ba7b67fb03145
simha <narasingam08@gmail.com>
2021-01-06 13:16:56 +0530

    added all fiels

---
+++ src/main/java/com/vodaphone/vodarecharge/VodaRechargeApplication.java
@@ -0,0 +1,14 @@
+package com.vodaphone.vodarecharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodaRechargeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodaRechargeApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/vodaphone/vodarecharge/VodaRechargeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodaphone.vodarecharge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodaRechargeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
